65376b2e81a1d66be1dcfcaca4ed4f6c14a5ec62
Thomas Howe <u1660884@unimail.hud.ac.uk>
2018-03-12 11:14:10 +0000

    Updates

---
+++ GEA_Sem2/Assets/DungGen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungGen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        int Xmax = 64;   //max number of x collums
+        int Ymax = 64;      // max number y row's
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
